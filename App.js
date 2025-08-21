7c0042ae7e61f72015d0fe5ab87ac531367b78ad
rohit <rohitmahto062@gmail.com>
2025-08-21 13:42:38 +0530

    Refactor reducer actions and update initial state naming

--- App.js
+++ App.js
@@ -2,16 +2,39 @@ import { createStore } from "redux";
 
 const initialState = {
   count: 0,
-  name: "Redux Example",
+  name: "initial state",
 };
+
+const INCREMENT = "count/increment";
+const DECREMENT = "count/decrement";
+const INCRESE_BY = "count/incrementBy";
+const DECRESE_BY = "count/decrementBy";
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "INCREMENT":
+    case INCREMENT:
       return {
         ...state,
         count: state.count + 1,
-        name: "Redux Example Updated",
+        name: "state Incremented",
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+        name: "state Decremented",
+      };
+    case INCRESE_BY:
+      return {
+        ...state,
+        count: state.count + action.payload,
+        name: "state Incremented by " + action.payload,
+      };
+    case DECRESE_BY:
+      return {
+        ...state,
+        count: state.count - action.payload,
+        name: "state Incremented by " + action.payload,
       };
 
     default:
@@ -20,7 +43,13 @@ const reducer = (state = initialState, action) => {
 };
 
 const store = createStore(reducer);
-console.log(store.getState());
+console.log(store);
 
-store.dispatch({ type: "INCREMENT" });
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({ type: INCREMENT });
+store.dispatch({ type: DECREMENT });
+store.dispatch({ type: INCRESE_BY, payload: 5 });
+store.dispatch({ type: DECRESE_BY, payload: 3 });
