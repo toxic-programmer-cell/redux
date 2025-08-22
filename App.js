91500ba8701cb31f08d91d9f58e56f85db4a00f8
rohit <rohitmahto062@gmail.com>
2025-08-22 12:57:38 +0530

    Implement code changes to enhance functionality and improve performance

--- App.js
+++ App.js
@@ -1,63 +1,28 @@
 import { createStore } from "redux";
+import { productList } from "./productList.js";
 
 const initialState = {
-  count: 0,
-  name: "initial state",
+  product: productList,
+  cartItem: [],
+  wishList: [],
 };
 
-const counterDiv = document.getElementById("counter");
-const button = document.getElementById("button");
+console.log(initialState);
 
-const INCREMENT = "count/increment";
-const DECREMENT = "count/decrement";
-const INCRESE_BY = "count/incrementBy";
-const DECRESE_BY = "count/decrementBy";
+const ADD_TO_CART = "cart/additem";
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case INCREMENT:
-      return {
-        ...state,
-        count: state.count + 1,
-        name: "state Incremented",
-      };
-    case DECREMENT:
-      return {
-        ...state,
-        count: state.count - 1,
-        name: "state Decremented",
-      };
-    case INCRESE_BY:
-      return {
-        ...state,
-        count: state.count + action.payload,
-        name: "state increased by " + action.payload,
-      };
-    case DECRESE_BY:
-      return {
-        ...state,
-        count: state.count - action.payload,
-        name: "state decresed by " + action.payload,
-      };
-
+    case ADD_TO_CART:
+      return { ...state, cartItem: [...state.cartItem, action.payload] };
     default:
       return state;
   }
 };
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.());
-console.log(store);
 
-store.subscribe(() => {
-  console.log(store.getState());
-  counterDiv.innerText = store.getState().count;
+store.dispatch({
+  type: "cart/additem",
+  payload: { productId: 1, quantity: 1 },
 });
-
-button.addEventListener("click", () => {
-  store.dispatch({ type: INCREMENT });
-});
-
-// store.dispatch({ type: INCREMENT });
-// store.dispatch({ type: DECREMENT });
-// store.dispatch({ type: INCRESE_BY, payload: 5 });
-// store.dispatch({ type: DECRESE_BY, payload: 3 });
